e7e37ee38ba953870e10c972fc312207e7a0e7d0
Jean Amadeu <jean.amadeu2@gmail.com>
2021-03-22 10:45:58 -0300

    Log ListenerDeTransacao

--- src/main/java/br/com/zup/transacao/ListenerDeTransacao.java
+++ src/main/java/br/com/zup/transacao/ListenerDeTransacao.java
@@ -1,21 +1,28 @@
 package br.com.zup.transacao;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ListenerDeTransacao {
 
+    private final Logger logger = LoggerFactory.getLogger(ListenerDeTransacao.class);
     private final TransacaoRepository transacaoRepository;
 
+    @Autowired
     public ListenerDeTransacao(TransacaoRepository transacaoRepository) {
         this.transacaoRepository = transacaoRepository;
     }
 
     @KafkaListener(topics = "${spring.kafka.topic.transactions}")
     public void ouvir(TransacaoResponse transacaoResponse) {
+        logger.info("Nova mensagem {}", transacaoResponse);
         Transacao transacao = transacaoResponse.toModel();
         transacaoRepository.save(transacao);
+        logger.info("Nova transacao {}", transacao);
         System.out.println(transacaoResponse);
     }
 }
